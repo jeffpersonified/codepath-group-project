2ad2262681b5deda62bd81f72012af40f3e92a60
Rebecca Goldman <rebecca@anyroad.com>
2015-06-18 01:16:27 -0700

    Add comments & remove extraneous lines

--- Circles2/BubbleScene.swift
+++ Circles2/BubbleScene.swift
@@ -1,81 +1,59 @@
-//
-//  BubbleScene.swift
-//  Sutra, Copyright (c) 2015 codepath. All rights reserved.
-//
-
-import SpriteKit
-
-class BubbleScene: SKScene {
+ //
+ //  BubbleScene.swift
+ //  Circles2
+ //
+ //  Created by Rebecca Goldman on 6/13/15.
+ //  Copyright (c) 2015 codepath. All rights reserved.
+ //
+ 
+ import SpriteKit
+ 
+ class BubbleScene: SKScene {
     
-    
-    var deltaPoint = CGPointZero
     var myLabel:SKLabelNode!
-
+    
     override func didMoveToView(view: SKView) {
-        
         self.physicsBody = SKPhysicsBody(edgeLoopFromRect: self.frame)
         
     }
     
-  
     
-//    override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
-//        
-//        for touch: AnyObject in touches{
-//            let location = touch.locationInNode(self)
-//            let sprite = self.nodeAtPoint(location)
-//            if self.nodeAtPoint(location) == sprite {
-//                
-//                sprite.removeFromParent()
-//            }
-//        }
-//    }
-
-
-    override func touchesMoved(touches: Set<NSObject>, withEvent event: UIEvent) {
-    
-    for touch: AnyObject in touches{
-    let location = touch.locationInNode(self)
-    let sprite = self.nodeAtPoint(location)
-        var currentPoint:CGPoint! = touch.locationInNode(self)
-        var previousPoint:CGPoint! = touch.previousLocationInNode(self)
-        sprite.position = CGPointMake(currentPoint.x - previousPoint.x, currentPoint.y - previousPoint.y)
-
-    }
-    
-    }
-    
+    // this function takes size/string/location parameters passed in the InputTransition
+    // and adds a new sprite to the Scene based on those parameters
     func addBubble(#size: CGSize, #string: String, #location: CGPoint) {
+        // ignore the error stating extraneous #
         
+        
+        // set a sprite with the image named "cicle"
         let sprite = SKSpriteNode(imageNamed:"circle")
+        
+        // set the sprite's size to the "size" parameter passed in InputTransition
         sprite.size = CGSize(width: size.width, height: size.height)
         sprite.xScale = 1.0
         sprite.yScale = 1.0
-        sprite.position = location
         
-        myLabel = SKLabelNode(fontNamed: "Arial")
+        sprite.position = location
+        // due to gravity, sprite automatically falls to bottom of screen
+        
+        // create a label that will take the "string" parameter passed in InputTransition
+        myLabel = SKLabelNode(fontNamed: "Helvetica Neue")
         myLabel.fontColor = UIColor.whiteColor()
         myLabel.text = string
         myLabel.fontSize = 17
-
-        println("\(myLabel)")
         
+        // this is where we can make the sprite do cool things
         var action = SKAction.scaleTo(0.8, duration: 0.6)
         sprite.runAction(SKAction.repeatActionForever(action))
         
-        
+        // specifying that the sprite is a circle that reacts with other sprites
+        // with some physics
         sprite.physicsBody = SKPhysicsBody(circleOfRadius:sprite.size.width/2)
         sprite.physicsBody!.dynamic = true
         
+        // add sprite to view & then add string to the sprite
         self.addChild(sprite)
         sprite.addChild(myLabel)
-
-    }
-   
-    override func update(currentTime: CFTimeInterval) {
+        
     }
     
-    
-
-    
-}
+ }
